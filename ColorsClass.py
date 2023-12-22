1a068257108943aa8feef2dae01ccfe387123e38
TheWrightDeveloper <125565506+TheWrightDeveloper@users.noreply.github.com>
2023-12-22 11:38:31 -0800

    Add files via upload

---
+++ ColorsClass.py
@@ -0,0 +1,19 @@
+class Colors:
+    """
+    Colors Class
+
+    List of ASCII colors
+    """
+    RESET = "\033[0m"
+    BOLD = "\033[01m"
+    UNDERLINE = "\033[04m"
+
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    CYAN = "\033[36m"
+    LIGHTGRAY = "\033[37m"
+    DARKGRAY = "\033[90m"
+    YELLOW = "\033[93m"
+    LIGHTBLUE = "\033[94m"
+    BLUE = "\033[34m"
+    LIGHTWHITE = "\033[1;37m"
